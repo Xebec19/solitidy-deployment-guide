3d7c324eafc771f023619d7e0eb823043513f069
xebec19 <rohandeveloper106@gmail.com>
2025-07-05 22:38:41 +0530

    test: add initial Campaign test setup

---
+++ test/Campaign.test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+
+const web3 = new Web3(ganache.provider());
+
+const campaign = require("../ethereum/build/Campaign.json");
+const campiledCampaign = require("../ethereum/build/Campaign.json");
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+});
